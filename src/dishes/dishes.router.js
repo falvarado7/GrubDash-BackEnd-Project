b89f0d67cdcb04f3d98a764a9bf80548038e06d4
Francisco <francisco.alvarado217@yahoo.com>
2024-02-03 23:35:09 -0600

    updated code

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -7,6 +7,9 @@ const controller = require("./dishes.controller");
     .route("/")
     .get(controller.list)
     .post(controller.create)
+
+  router
+    .route("/:dishId")
     .get(controller.read)
     .put(controller.update)
 
