75440a5d63221177ca007cf7193a0588d012dd58
Francisco <francisco.alvarado217@yahoo.com>
2024-02-04 12:08:25 -0600

    updated code in file

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -65,12 +65,49 @@ function dishExists(req, res, next) {
   })
 }
 
+// Validation Function for the Update function:
+function bodyIdMatchesRouteId(req, res, next) {
+  const dishId = res.locals.dishId;
+  const reqBody = res.locals.reqBody;
+
+  if (reqBody.id) {
+    if (reqBody.id === dishId) {
+      return next();
+    }
+
+    next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${reqBody.id}, Route: ${dishId}`,
+    });
+  }
+
+  return next();
+}
+
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function update(req,res) {
+  const dish = res.locals.dish;
+  const reqBody = res.locals.reqBody;
+
+  // Creating array of property names
+  const existingDishProperties = Object.getOwnPropertyNames(dish);
+
+  for (let i = 0; i < existingDishProperties.length; i++) {
+    // Accessing each dish object key within the array
+    let propName = existingDishProperties[i];
+    // Updating each value if there is a difference between the existing dish and the req body dish
+    if (dish[propName] !== reqBody[propName]) {
+      dish[propName] = reqBody[propName];
+    }
+  }
+  res.json({ data: dish });
+}
+
 function create(req, res) {
   const reqBody = res.locals.reqBody;
   const newDish = {
     ...reqBody,
-    id: nextId,
+    id: nextId(),
   };
   dishes.push(newDish);
   res.status(201).json({ data: newDish })
@@ -80,10 +117,6 @@ function read(req, res) {
   res.status(200).json({ data: res.locals.dish})
 }
 
-function update(req,res) {
-
-}
-
 function list(req, res) {
   res.status(200).json({ data: dishes });
 };
@@ -91,6 +124,6 @@ function list(req, res) {
 module.exports = {
   create: [bodyHasNameProperty, bodyHasDescProperty, bodyHasPriceProperty, bodyHasImgUrl, create],
   read: [dishExists, read],
-  update,
+  update: [dishExists, bodyHasNameProperty, bodyHasDescProperty, bodyHasPriceProperty, bodyHasImgUrl, bodyIdMatchesRouteId, update,],
   list,
 }
